9419e5538151af9165dffd0c55ef15c3423f3495
junhyun87 <pe.jeon87@gmail.com>
2019-08-06 01:31:45 +0900

    React Native 코딩 시작 - App.js와 Count.js 나누어 두개 파일에서 서로 export import 하여 참조

--- App.js
+++ App.js
@@ -1,71 +1,22 @@
 import React from 'react';
-import {View, Button, Text, ScrollView, StyleSheet, Switch} from 'react-native'
-import {Constants} from 'expo'
+import { StyleSheet, Text, View } from 'react-native';
+import {Count} from './Count.js'
 
-const styles = StyleSheet.create({
-  appContainer: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  count: {
-    fontSize: 48,
-  }
-})
-
-class Counter extends React.Component {
-  constructor() {
-    super()
-    this.state = {
-      count: 0,
-    }
-  }
-
-  componentDidMount() {
-    this.interval = setInterval(this.inc, 1000)
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.interval)
-  }
-
-  inc = () => {
-    this.setState(prevState => ({
-      count: prevState.count + 1,
-    }))
-  }
-
+export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.appContainer}>
-        <Text style={styles.count}>{this.state.count}</Text>
+      <View style={styles.container}>
+        <Count count={0}/>
       </View>
-    )
+    );
   }
 }
 
-export default class App extends React.Component { 
-  constructor(props) {
-    super(props)
-    this.state = {
-      showCounter: true,
-    }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor : '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
   }
-
-  toggleCounter = () => this.setState(prevState => ({
-    showCounter: !prevState.showCounter,
-  }))
-
-  render() {
-    if (this.state.showCounter) {
-      return (
-        <View style={styles.appContainer}>
-        <Button title="toggle" onPress={this.toggleCounter} />
-        <Counter />
-        </View>
-      )
-    } else {
-      return <Button title="toggle" onPress={this.toggleCounter}/>
-    }
-  }
-}
+});
